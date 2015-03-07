bd359e9b84890e797825b9454b4c3129698fb3cc
Josema0 <cosemine.josema@gmail.com>
2015-03-07 10:28:48 -0600

    Modificacion simple

--- Disco.java
+++ Disco.java
@@ -15,30 +15,30 @@ public class Disco{
 		} catch (Exception e) {
 			System.out.println("Caracter inválido");
 		}
-    }
+	}
     
-    private static void getMenu(){
+	private static void getMenu(){
 		System.out.print("Qué desea hacer, con el numero correspondiente, elija una opcion del menu: "+ "\n" +
-                            "1) Crear un Registro"+ "\n"+
-                            "2) Eliminar un Registro"+"\n"+
-                            "3) Imprimir un Registro"+"\n"+
-                            "4) Salir del programa"+"\n"+
-                            "================ Opción:");
-    }
+							"1) Crear un Registro"+ "\n"+
+							"2) Eliminar un Registro"+"\n"+
+							"3) Imprimir un Registro"+"\n"+
+							"4) Salir del programa"+"\n"+
+							"================ Opción:");
+	}
     
     public static void setMenu(int option){
-        if(option == 1){
-            System.out.println("--------------------------------------------------------------");
-        }else if(option == 2){
-            System.out.println("--------------------------------------------------------------");
-        }else if(option == 3){
+		if(option == 1){
+			System.out.println("--------------------------------------------------------------");
+		}else if(option == 2){
+			System.out.println("--------------------------------------------------------------");
+		}else if(option == 3){
 			System.out.println("--------------------------------------------------------------");
 		}else if(option == 4){
-            System.out.println("--------------------------------------------------------------");
-            System.out.println("Salir, Adios");
-        }else{
-            System.out.println("--------------------------------------------------------------");
-            System.out.println("Error, Opción invalida");
-        }
-    }
+			System.out.println("--------------------------------------------------------------");
+			System.out.println("Salir, Adios");
+		}else{
+			System.out.println("--------------------------------------------------------------");
+			System.out.println("Error, Opción invalida");
+		}
+	}
 }
