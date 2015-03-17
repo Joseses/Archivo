e790e6ba64967c17a2209aedeba24191c92d3f5e
JoseOm07 <beto_bcw_09@hotmail.com>
2015-03-17 11:41:49 -0600

    lo mismo pero con otro nombre

---
+++ SecuencialFijo/Registro_Sec.java
@@ -0,0 +1,123 @@
+import java.io.*;
+import java.lang.*;
+
+public class Registro_Sec {
+    
+        private byte[] sucursal = new byte[20];
+	private int numero = 0;
+	private byte[] nombre = new byte[20];
+	private double saldo = 0;
+	private byte[] eliminado = new byte[1];
+       // private byte[] puntero = new byte[1];
+    
+    /*-----------------------------------------------------------------
+    / constructores
+    /-----------------------------------------------------------------*/
+    
+	public Registro_Sec() {}
+    
+	public Registro_Sec( String nomSucursal, int numCuenta,
+                     String nomCliente, double deposito )
+	{
+		byte[] chars;
+        
+		if( nomSucursal.length() > 20 || nomCliente.length() > 20 ) {
+            
+			System.out.println( "ATENCION: Sucursal o nombre con más de 20 caracteres" );
+                }
+        
+		for( int i = 0; i < 20 && i < nomSucursal.getBytes().length; i++ ){
+			sucursal[i] = nomSucursal.getBytes()[i];
+		}
+		numero = numCuenta;
+		for( int i = 0; i < 20 && i < nomCliente.getBytes().length; i++ ){
+			nombre[i] = nomCliente.getBytes()[i];
+		}
+		String temp = "n";
+		eliminado[0] = temp.getBytes()[0];
+		saldo = deposito;
+                
+                temp = "0";
+//                puntero[0]=temp.getBytes()[0];
+	}
+    
+    /*-----------------------------------------------------------------
+    / métodos getters
+    /-----------------------------------------------------------------*/
+    
+	public String getSucursal() { return new String( sucursal ); }
+    
+	public int getNumero()      { return numero; }
+    
+	public String getNombre()   { return new String( nombre ); }
+    
+	public double getSaldo()    { return saldo; }
+	
+	public String getEliminado() { return new String( eliminado ); }
+        
+        //public String getPuntero() { return new String(puntero); }
+    
+    /*-----------------------------------------------------------------
+    / longitud en bytes de un registro
+    /-----------------------------------------------------------------*/
+    
+	public int length() {
+        
+		return sucursal.length +
+               Integer.SIZE / 8 +
+               nombre.length +
+               Double.SIZE / 8 +
+               eliminado.length; 
+              //+ puntero.length;
+	}
+    
+    /*-----------------------------------------------------------------
+    / métodos para escribir y leer un registro
+    /-----------------------------------------------------------------*/
+    
+	public void read( RandomAccessFile raf ) throws IOException {
+        
+		
+		raf.read( sucursal );
+		numero = raf.readInt();
+		raf.read( nombre );
+		saldo = raf.readDouble();
+                raf.read( eliminado );
+//                raf.read(puntero);
+	}
+    
+	public void write( RandomAccessFile raf ) throws IOException {
+        
+		
+		raf.write( sucursal );
+		raf.writeInt( numero );
+		raf.write( nombre );
+		raf.writeDouble( saldo );
+                raf.write( eliminado );
+//                raf.write( puntero);
+	}
+	//Función para marcar registros...
+	public void erase( RandomAccessFile raf) throws IOException {
+		String temp = "y";
+		eliminado[0] = temp.getBytes()[0]; //Escribimos "y" para marcar el registro como eliminado
+                raf.write( eliminado );
+		raf.write( sucursal );
+		raf.writeInt( numero );
+		raf.write( nombre );
+		raf.writeDouble( saldo );
+//                raf.write( puntero);
+		System.out.println("[REGISTRO - erase] " + getEliminado());
+	}
+        
+       /* public void puntear(RandomAccessFile raf,int p) throws IOException {
+            String temp = Integer.toString(p);
+//            puntero[0] = temp.getBytes()[0];
+         
+            raf.write( sucursal );
+		raf.writeInt( numero );
+		raf.write( nombre );
+		raf.writeDouble( saldo );
+                raf.write( eliminado );    
+            raf.write( puntero);
+        }*/
+}
